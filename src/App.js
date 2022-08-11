f9108f7603f009cd59b46ca84c9a7e41c8c4509b
SOFIA DOMINGUEZ GOMEZ <sofiadominguezgomez03@gmail.com>
2022-08-11 10:56:25 -0300

    Configure React app for deployment to Github Pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import satisfied from './assets/satisfied.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={satisfied} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi primer proyecto con React
         </p>
         <a
           className="App-link"
